04d83e7954e9ea1edf452a946a1892af9cbc659f
Anthony Moore <63217258+amoor131@users.noreply.github.com>
2022-08-18 21:20:23 -0400

    update
    improved comments

--- latestVersion/unscramble2.py
+++ latestVersion/unscramble2.py
@@ -4,7 +4,7 @@
 # 6/05/22
 #
 # What it does:
-# This code make a dictionary from a JSON file that will have key values 
+# Searches a json file to see if there is a key that matches users input, returns key's value if found
 #
 # Possible improvements:
 # 
@@ -17,35 +17,24 @@ def main():
     new_line = '\n'
     indent = '   '
     
-    #DEBUG PRINT
-    #print("==============start==============")
+    #print("==============start==============") #DEBUG PRINT
     with open("dictionary.json","r") as dictionary:
         dict = json.load(dictionary)
         #DEBUG PRINT
-        print(f"dictionary length: {len(dict)}")
+        #print(f"dictionary length: {len(dict)}")
         query = "a" #init value arbitrary
         #DEBUG PRINT
         print("see what words you can make with any set of letters!")
         while query != "q":
             query = input()
             orderedQuery = ''.join(sorted(query)).upper() #.strip('\n')
-            #DEBUG PRINT
-            print(f"orderedQuery = {orderedQuery}")
+            #print(f"orderedQuery = {orderedQuery}") #DEBUG PRINT
             #print(f"{dict.keys()}")
             if orderedQuery in dict.keys():
                     print(f"{dict[orderedQuery]}")
             else:
                 print(f"No words can be made from {query}")
-            '''
-            try:
-                if orderedQuery in dict.keys():
-                    print(f"{dict[orderedQuery]}")
-                    print("exists")
-            except:
-                print("No words can be made")
-            '''
             print(f"Enter another set to test for or enter q to quit")
-    #DEBUG PRINT
-    #print("===============end===============")
+    #print("===============end===============") #DEBUG PRINT
 if __name__ == "__main__":
     main()
