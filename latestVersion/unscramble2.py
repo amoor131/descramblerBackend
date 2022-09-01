79cf8f3ea0f3a164756290fdca5017178639b528
Anthony Moore <63217258+amoor131@users.noreply.github.com>
2022-09-01 10:01:05 -0400

    added addKey and fitsIn functions
    I've added some of the new functions in the updated version of this project but haven't finished making it all work. It's currently broken, I'm hoping to finish updating this version soon.

--- latestVersion/unscramble2.py
+++ latestVersion/unscramble2.py
@@ -12,6 +12,41 @@
 
 import json
 
+def fitsIn(testWord, keyWord):
+    comp = list(keyWord)
+    if len(testWord) <= len(keyWord):
+        for letter in testWord.strip('\n'):
+            #print(f"letter: {letter}| keyWord: {keyWord}") #DEBUG
+            if letter in comp:
+                comp[comp.index(letter)] = '#'
+            else:
+                return False
+        #only returns true if all letters were found in keyWord
+        return True
+
+def addKey(testKey):
+    print(f"testing key: {testKey}")
+    #used to add new lines to f statements
+    new_line = '\n'
+    #check that the entry is only letters
+    if testKey.isalpha() == False:
+        return -1
+    #double check that the user input isn't already a key in the database
+    if testKey in descrambler.keys():
+        return 0
+    #user input in reverse alphabetical order, 
+    sortedKey = ''.join(sorted(testKey.strip('\n')))
+    foundWords = ''
+    with open('sorted_length.txt','r') as engWords:
+        for word in engWords:
+            if fitsIn(word, testKey):
+                foundWords += ' ' + ''.join(word.strip('\n')) # not sure if I need to strip new line
+        if len(foundWords) > 0:
+            entry = Sort(key=testKey, words=foundWords)
+            db.session.add(entry)
+            db.session.commit()
+            return 1
+
 def main():
     dict = {}
     new_line = '\n'
@@ -33,7 +68,15 @@ def main():
             if orderedQuery in dict.keys():
                     print(f"{dict[orderedQuery]}")
             else:
-                print(f"No words can be made from {query}")
+                tryAddKey = addKey(letterSet)
+                match tryAddKey:
+                    case 1:
+                        print(f"tried adding key {letterSet} but it already existed")
+                    case 0:
+                        print(f"tried adding key {letterSet} but it already existed")
+                    case -1:
+                        print(f"tried adding key {letterSet} but it contains non alphabet characters")
+                #print(f"No words can be made from {query}")
             print(f"Enter another set to test for or enter q to quit")
     #print("===============end===============") #DEBUG PRINT
 if __name__ == "__main__":
